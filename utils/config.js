525209eee286b36b46c23d7364e7d6f7ba1875eb
rjw <i@jianwei.ren>
2020-02-23 12:03:46 +0800

    精简代码

--- utils/config.js
+++ utils/config.js
@@ -1,15 +1,3 @@
-/*
- * 
- * WordPres版微信小程序
- * author: jianbo
- * organization: 守望轩  www.watch-life.net
- * github:    https://github.com/iamxjb/winxin-app-watch-life.net
- * 技术支持微信号：iamxjb
- * 开源协议：MIT
- * Copyright (c) 2017 https://www.watch-life.net All rights reserved.
- */
-
-
 
 //配置域名,域名只修改此处。
 //如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
